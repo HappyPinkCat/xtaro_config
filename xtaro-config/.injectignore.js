d3659c6da406db193fc44d67585ce9fbe76be0e5
lpgong <lpgong@trip.com>
2024-03-01 14:15:20 +0800

    chore: init xtaro project

---
+++ xtaro-config/.injectignore.js
@@ -0,0 +1,17 @@
+module.exports = [
+    '.swc/',
+    "plugins/",
+    "config/",
+    "dist/",
+    "src/taroCwx/",
+    "src/app.config.js",
+    "src/app.js",
+    "src/app.js",
+    "webpack.common.js",
+    "webpack.common.origin.js",
+    'babel.config.js',
+	'commonModule.js',
+	'commonModule.prod.js',
+	'global.d.ts',
+	'.injectignore.js'
+];
---
+++ xtaro-config/src/app.config.crn.ts
@@ -0,0 +1,15 @@
+//必须使用node端可以运行的语法！！！！
+module.exports = {
+  pages: [
+    "pages/xtaro-config/index",
+    "pages/mainPage/index",
+    "pages/xtaro-config/subPackages/foo/index",
+    "pages/xtaro-config/subPackages/bar/index",
+  ],
+  window: {
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black",
+  },
+};
